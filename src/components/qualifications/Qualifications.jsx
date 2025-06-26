c223962c0c1201252ec305afeecfb9da3018a7a0
Nagoloum <152168738+Nagoloum@users.noreply.github.com>
2025-06-26 21:28:35 +0100

    new

---
+++ src/components/qualifications/Qualifications.jsx
@@ -0,0 +1,160 @@
+import React from 'react'
+import "./Qualifications.css";
+
+const Qualifications = () => {
+    return (
+        <section className="qualification section">
+            <h2 className="section__title">Mes Qualifications</h2>
+            <span className="section__subtitle">Mon parcours personnel</span>
+
+            <div className="qualification__container caontainer">
+                <div className="qualification__tabs">
+                    <div className="qualification__button qualification__active button--flex">
+                        <i className="uil uil-graduation-cap qualification__icon"></i> Education
+                    </div>
+
+                    <div className="qualification__button button--flex">
+                        <i className="uil uil-briefcase-alt qualification__icon"></i> Experience
+                    </div>
+                </div>
+
+                <div className="qualification__sections">
+                    <div className="qualification__content">
+                        <div className="qualification__data">
+                            <div>
+                                <h3 className="qualification__title">Web Design</h3>
+                                <span className="qualification__subtitle">GTES</span>
+                                <div className="qualification__calendar">
+                                    <i className="uil uil-calendar-alt"></i> 2024 - Present
+                                </div>
+                            </div>
+
+                            <div>
+                                <span className="qualification__rounder"></span>
+                                <span className="qualification__line"></span>
+
+                            </div>
+                        </div>
+
+                        <div className="qualification__data">
+
+                            <div>
+
+                            </div>
+
+                            <div>
+                                <span className="qualification__rounder"></span>
+                                <span className="qualification__line"></span>
+                            </div>
+
+                            <div>
+                                <h3 className="qualification__title">Art Director</h3>
+                                <span className="qualification__subtitle">GTES</span>
+                                <div className="qualification__calendar">
+                                    <i className="uil uil-calendar-alt"></i> 2024 - Present
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <div className="qualification__data">
+                            <div>
+                                <h3 className="qualification__title">Web Development</h3>
+                                <span className="qualification__subtitle">GTES</span>
+                                <div className="qualification__calendar">
+                                    <i className="uil uil-calendar-alt"></i> 2024 - Present
+                                </div>
+                            </div>
+
+                            <div>
+                                <span className="qualification__rounder"></span>
+                                <span className="qualification__line"></span>
+
+                            </div>
+                        </div>
+
+                        <div className="qualification__data">
+
+                            <div>
+
+                            </div>
+
+                            <div>
+                                <span className="qualification__rounder"></span>
+                                <span className="qualification__line"></span>
+                            </div>
+
+                            <div>
+                                <h3 className="qualification__title">UX Expert</h3>
+                                <span className="qualification__subtitle">GTES</span>
+                                <div className="qualification__calendar">
+                                    <i className="uil uil-calendar-alt"></i> 2024 - Present
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div className="qualification__content">
+                        <div className="qualification__data">
+                            <div>
+                                <h3 className="qualification__title">Web Design</h3>
+                                <span className="qualification__subtitle">GTES</span>
+                                <div className="qualification__calendar">
+                                    <i className="uil uil-calendar-alt"></i> 2024 - Present
+                                </div>
+                            </div>
+
+                            <div>
+                                <span className="qualification__rounder"></span>
+                                <span className="qualification__line"></span>
+
+                            </div>
+                        </div>
+
+                        <div className="qualification__data">
+
+                            <div>
+
+                            </div>
+
+                            <div>
+                                <span className="qualification__rounder"></span>
+                                <span className="qualification__line"></span>
+                            </div>
+
+                            <div>
+                                <h3 className="qualification__title">Art Director</h3>
+                                <span className="qualification__subtitle">GTES</span>
+                                <div className="qualification__calendar">
+                                    <i className="uil uil-calendar-alt"></i> 2024 - Present
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <div className="qualification__data">
+                            <div>
+                                <h3 className="qualification__title">Web Development</h3>
+                                <span className="qualification__subtitle">GTES</span>
+                                <div className="qualification__calendar">
+                                    <i className="uil uil-calendar-alt"></i> 2024 - Present
+                                </div>
+                            </div>
+
+                            <div>
+                                <span className="qualification__rounder"></span>
+                                <span className="qualification__line"></span>
+
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+        </section>
+    )
+}
+
+export default Qualifications

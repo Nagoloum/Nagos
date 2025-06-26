9a003e6323048b1153d57ee8f940170250259ff8
Nagoloum <152168738+Nagoloum@users.noreply.github.com>
2025-06-26 21:40:21 +0100

    new

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Home from './components/home/Home';
 import About from './components/about/About';
 import Skills from './components/skills/Skills';
 import Services from './components/services/Services';
-import Qualifications from './components/qualifications/Qualifications.JSX';
+import Qualifications from './components/qualifications/Qualifications';
 
 function App() {
   return (
